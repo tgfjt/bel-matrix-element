3b24f3dc7f3f6921c75bd57eb09e2571cbdaffc9
tgfjt <tgfjt.mail@gmail.com>
2017-04-20 19:59:46 +0900

    tweaks

--- docs/bundle.js
+++ docs/bundle.js
@@ -8,21 +8,31 @@ const app = module.exports = function() {
 
   function render(pinPos) {
     function clickHover(pos) {
-      console.log('Hover', pos)
+      // console.log('Hover', pos)
     }
 
-    function clickHandle(pos) {
+    function handleClick(pos) {
       morphdom(el, render(pos))
+    }
+
+    function handleSubmit(e) {
+      e.preventDefault();
+
+      console.log('importance', form.importance.value)
+      console.log('urgency', form.urgency.value)
     }
 
     const pinStyle = `top:${(pinPos.y - 1) * 16}px;left:${(pinPos.x - 1) * 16}px;`
     const pin = bel`<span class="pin" style="${pinStyle}">📍</span>`
 
     return bel`<div class="app">
-      ${matrix(160, clickHover, clickHandle)}
+      ${matrix(160, clickHover, handleClick)}
       ${pin}
-      <input type="range" name="importance" min="1" max="20" step="1" value=${pinPos.x} />
-      <input type="range" name="urgency" min="5" max="100" step="5" value=${(20 - pinPos.y) * 5} />
+      <form id="form" onsubmit=${handleSubmit}>
+        <input type="range" name="importance" min="1" max="20" step="1" value=${pinPos.x} />
+        <input type="range" name="urgency" min="5" max="100" step="5" value=${(21 - pinPos.y) * 5} />
+        <button type="submit">Send</button>
+      </form>
     </div>`
   }
 
